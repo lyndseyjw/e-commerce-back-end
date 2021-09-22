9c6e44a951bc84f9e98f80b9e7701126bece0cc6
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-09-22 05:48:17 -0700

    forgot to commit yesterday.. downloaded all node packages, created dotenv file, set up models, & began routes

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll( {
+      include: [{model: Product, through: ProductTag, as: 'tag_product' }]
+    });
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product, through: ProductTag, as: 'tag_product' }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  req.body = {
+
+    tag_name: req.body.tag_name,
+    productIds: [req.body.product_id],
+  }
+  Product.create(req.body)
+    .then((tag) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.productIds.length) {
+        const productTagIdArr = req.body.productIds.map((product_id) => {
+          return {
+            tag_id: tag.id,
+            product_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(tag);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
